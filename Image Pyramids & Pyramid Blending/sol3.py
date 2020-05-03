e41e52f8f6d7630a4c30a3d8274e7c126e0afc05
ben7z <ben.cohen3@mail.huji.ac.il>
2020-05-03 11:00:27 +0300

    Cleaning code, adding plot options..

--- Image Pyramids & Pyramid Blending/sol3.py
+++ Image Pyramids & Pyramid Blending/sol3.py
@@ -1,191 +1,159 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as scs
 from scipy.misc import imread
 import scipy.ndimage as ni
 from skimage.color import rgb2gray
 from scipy.signal import convolve2d as conv
+from enum import Enum
 import os
 
-#------------------------------prv function-----------------------------------------#
-#function : return image at gray or RGB
-def read_image(filename,representation): #filename-> the picture name,
-    # representation 1 for gray 2 for RGB
-    im =imread(filename)#catching the picture
-    im_float=im.astype(np.float64)#changing to float 64 type
-    im_float /=255 #normilaize
-    if (representation==1): #turning gray and return
-        im_g = rgb2gray(im_float)
+NORMALIZATION = 255
+MIN_Y_SHAPE_SIZE = 32
+MIN_X_SHAPE_SIZE = 32
+
+
+class Representation(Enum):
+    RGB = 1
+    GRAY = 2
+
+
+def read_image(filename, representation):
+    image = imread(filename)
+    image_float = image.astype(np.float64)
+    image_float /= NORMALIZATION
+
+    if representation == Representation.RGB:
+        im_g = rgb2gray(image)
         return im_g
 
-    elif (representation==2): #return RGB
-        return im_float
+    elif representation == Representation.GRAY:
+        return image_float
+    else:
+        raise ValueError('Wrong representation argument')
 
 
 def create_kernel(kernel_size):
+    counter = kernel_size
+    kernel = np.array([1]).reshape(1, 1).astype('float64')
 
-    counter = kernel_size#counter for while loop
-    kernel = np.array([1]).reshape(1, 1).astype('float64')#the first
-
-    while (counter > 1):
+    while counter > 1:
         a = np.array([1, 1]).reshape(1, 2).astype('float64')
-        kernel = (conv(kernel, a))#conveltion to get the binomial line
+        kernel = (conv(kernel, a))
         counter = counter - 1
-    gaus = kernel
-    gaus = gaus / np.sum(gaus)  # normilaze the kernel
-    return gaus
-#------------------------------prv function-----------------------------------------#
-##################################3.1################################################
+    return kernel / np.sum(kernel)
 
 
 def build_gaussian_pyramid(im, max_levels, filter_size):
-    """creating gaussian pyrmid
+    pyr = [im]
+    filter_vec = create_kernel(filter_size)
+    image = im
+    imx_shape = image.shape[0]
+    imy_shape = image.shape[1]
+    max_value = max_levels
 
-    :param im: the image
-    :param max_levels: the max level we allowed for bulding the pyr
-    :param filter_size: the size of our filter
-    :return:filter vector of the gauss bin and an array of gaussian pyrmid
-    """
-    pyr=[]#array of the pyr
-    pyr.append(im)
-    filter_vec= create_kernel(filter_size)#creating the filter
-    ###reduce
-    new_im=im
-    imx_shape = new_im.shape[0]
-    imy_shape =new_im.shape[1]
-    my_max=max_levels
-    while ((imx_shape>=32 and imy_shape>=32) and my_max>1):
-        new_im= reduce(filter_vec,new_im)
-        pyr.append(new_im)
-        imx_shape = new_im.shape[0]
-        imy_shape = new_im.shape[1]
-        my_max-=1
-    return pyr,filter_vec
+    while (imx_shape >= MIN_X_SHAPE_SIZE and imy_shape >= MIN_Y_SHAPE_SIZE) and max_value > 1:
+        image = reduce(filter_vec, image)
+        pyr.append(image)
+        imx_shape = image.shape[0]
+        imy_shape = image.shape[1]
+        max_value -= 1
+    return pyr, filter_vec
+
 
 def build_laplacian_pyramid(im, max_levels, filter_size):
-    '''
-    bulding laplacing pyramid by expanding and to subtract from the gaussian pyramid
-    :param im: the original image
-    :param max_levels: the max level we allowed for bulding the pyr
-    :param filter_size: the size of our filter
-    :return: filter vector of the gauss bin and an array of laplacian pyrmid
-    '''
-    ga_pyr,filter_vec= build_gaussian_pyramid(im,max_levels,filter_size)#getting the gaussian pyr
-    pyr=[] #initalize our arrray
-    for i in range(len(ga_pyr)-1):#creating the laplican pyrmid
-        new_im= expand(ga_pyr[i],ga_pyr[i+1],filter_vec)
-        my_lap=ga_pyr[i]-new_im# creating the laplican matrix
-        pyr.append(my_lap)#adding to our array
-    pyr.append(ga_pyr[-1]) #adding the lasone
-    return pyr,filter_vec
-#**********************************help function**************************************
-def reduce(filter_vec,im):
-    new_im=im
-    new_im = ni.filters.convolve(new_im, filter_vec)  # bluring by x
-    new_im = ni.filters.convolve(new_im, filter_vec.T)  # bluring by y
-    new_im = new_im[::2, ::2]  # reducing
-    return new_im
+    gaussian_pyramid, filter_vec = build_gaussian_pyramid(im, max_levels, filter_size)
+    pyr = []
 
-def expand(imB,imS,filter_vec):
-    my_zeros = np.zeros(imB.shape)  # zero matrix in the shape of the g(i)
-    my_zeros[::2, ::2] = imS  # the expand
-    new_im = ni.filters.convolve(my_zeros, 2 * filter_vec)  # blure on x
-    new_im = ni.filters.convolve(new_im, (2 * filter_vec).T)  # blure on y
-    return new_im
+    for i in range(len(gaussian_pyramid) - 1):
+        image = expand(gaussian_pyramid[i], gaussian_pyramid[i + 1], filter_vec)
+        laplacian = gaussian_pyramid[i] - image
+        pyr.append(laplacian)
+    pyr.append(gaussian_pyramid[-1])
+    return pyr, filter_vec
 
 
-#**********************************help function**************************************
-##########################3.2######################################################
+def reduce(filter_vec, im):
+    image = im
+    # blurring x and y axis
+    image = ni.filters.convolve(image, filter_vec)
+    image = ni.filters.convolve(image, filter_vec.T)
+    # reducing
+    return image[::2, ::2]
+
+
+def expand(im_b, im_s, filter_vec):
+    zeros_arr = np.zeros(im_b.shape)
+    zeros_arr[::2, ::2] = im_s
+    image = ni.filters.convolve(zeros_arr, 2 * filter_vec)
+    image = ni.filters.convolve(image, (2 * filter_vec).T)
+    return image
 
 
 def laplacian_to_image(lpyr, filter_vec, coeff):
-    '''
+    pyr_length = len(lpyr)
+    base_image = coeff[pyr_length - 1] * lpyr[pyr_length - 1]
 
-    :param lpyr: our laplicina pyrmid
-    :param filter_vec: our filter vecotre
-    :param coeff: in the length of our pyrmid , muliply with our laplican and adding
-    :return: image which created by adding the laplacian (after expand) to each other.
+    for i in range(pyr_length - 1):
+        k = pyr_length - (i + 1)
+        base_image = expand(lpyr[k - 1], base_image, filter_vec)
+        base_image += lpyr[k - 1] * coeff[k - 1]
+    return base_image
 
-    '''
-    my_len=len(lpyr)
-    img=coeff[my_len-1]*lpyr[my_len-1]#my base image
-    for i in range(my_len - 1):
-        k=my_len-(i+1)
-        img=expand(lpyr[k-1],img,filter_vec)#expanding
-        img+=lpyr[k-1]*coeff[k-1]#summing
-    return img
-
-
-##############################3.3########################################
 
 def render_pyramid(pyr, levels):
-    '''
+    height = pyr[0].shape[0]
+    length = 0
+    my_level = levels - 1
 
-    :param pyr:the pyrmid we want to display
-    :param levels: the amound of pictures to display
-    :return:pictuer which holding the all pyramid
-    '''
-    hight= pyr[0].shape[0]#getting the hight
-    length=0
-    my_level=levels-1
-    while my_level>=0:
-        length+=pyr[my_level].shape[1]#getting the length
-        my_level=my_level-1
-    ren=np.zeros((hight,length))#our return
+    while my_level >= 0:
+        length += pyr[my_level].shape[1]
+        my_level = my_level - 1
+    rendered_image = np.zeros((height, length))
     col = 0
-    for i in range(levels):
-        my_stretch = (pyr[i] - np.min(pyr[i])) / (np.max(pyr[i]) - np.min(pyr[i]))#streching
-        ren[:my_stretch.shape[0],col:col+my_stretch.shape[1]]= my_stretch#combine the pyramid
-        col+=my_stretch.shape[1]
-    return ren
 
-def display_pyramid(pyr,levels):
-    im=render_pyramid(pyr, levels)#getting the image we want to display
-    plt.imshow(im, cmap=plt.gray())
+    for i in range(levels):
+        my_stretch = (pyr[i] - np.min(pyr[i])) / (np.max(pyr[i]) - np.min(pyr[i]))
+        rendered_image[:my_stretch.shape[0], col:col + my_stretch.shape[1]] = my_stretch
+        col += my_stretch.shape[1]
+    return rendered_image
+
+
+def display_pyramid(pyr, levels):
+    image = render_pyramid(pyr, levels)
+    plt.imshow(image, cmap=plt.gray())
     plt.show()
 
+
 def pyramid_blending(im1, im2, mask, max_levels, filter_size_im, filter_size_mask):
-    '''
+    laplacian1, v1 = build_laplacian_pyramid(im1, max_levels, filter_size_im, )
+    laplacian2, v2 = build_laplacian_pyramid(im2, max_levels, filter_size_im, )
+    mask = mask.astype('float64')
+    gaussian_mask, vm = build_gaussian_pyramid(mask, max_levels, filter_size_mask)
+    pyr = []
 
-    :param im1:first image we want to blend
-    :param im2:seconed image we want to blind
-    :param mask:our mask
-    :param max_levels: the max level we want to get
-    :param filter_size_im: size of the filter for gaussian and laplican pyramid
-    :param filter_size_mask: size of filter for the mask pyramid
-    :return:blend image
-    '''
-    L1,v1=build_laplacian_pyramid(im1,max_levels,filter_size_im,)#laplcianan for first image
-    L2,v2=build_laplacian_pyramid(im2,max_levels,filter_size_im,)#laplacian for seconed image
-    mask=mask.astype('float64')#getting the mask
-    Gm,vm=build_gaussian_pyramid(mask,max_levels,filter_size_mask)#getting the gaussian for mask
-    pyr=[]
-    for i in range(len(L1)):
-        L3=Gm[i]*L1[i]+(1-Gm[i])*L2[i]#creating the blend pyramid
-        pyr.append(L3)
-    coeff=np.ones(len(L3))
-    img=np.clip(laplacian_to_image(pyr,v1,coeff),0,1)#bulding the image
-    return img
+    for i in range(len(laplacian1)):
+        laplacian_iter = gaussian_mask[i] * laplacian1[i] + (1 - gaussian_mask[i]) * laplacian2[i]
+        pyr.append(laplacian_iter)
+
+    coeff = np.ones(len(laplacian_iter))
+    return np.clip(laplacian_to_image(pyr, v1, coeff), 0, 1)
 
 
-####################################################################################################
 def relpath(filename):
     return os.path.join(os.path.dirname(__file__), filename)
 
-def blending_example1():
-    im1 = read_image(relpath('externals/im1.jpg'), 2)
-    im2= read_image(relpath('externals/im2.jpg'), 2)
-    mask=read_image(relpath('externals/mask.jpg'),1).astype(np.bool)
-    blend_im=np.zeros(im1.shape)
-    im1_R=im1[:,:,0]
-    im1_G=im1[:,:,1]
-    im1_B=im1[:,:,2]
-    im2_R = im2[:, :, 0]
-    im2_G = im2[:, :, 1]
-    im2_B = im2[:, :, 2]
-    blend_im[:,:,0],blend_im[:,:,1],blend_im[:,:,2]= pyramid_blending(im1_R,im2_R,mask,10,7,5),\
-                                                     pyramid_blending(im1_G,im2_G,mask,10,7,5),\
-                                                     pyramid_blending(im1_B,im2_B,mask,10,7,5)
+
+def blending_example(image1_path, image2_path, mask_path):
+    im1 = read_image(relpath(image1_path), Representation.GRAY)
+    im2 = read_image(relpath(image2_path), Representation.GRAY)
+    mask = read_image(relpath(mask_path), Representation.RGB).astype(np.bool)
+    blend_im = np.zeros(im1.shape)
+    im1_red, im1_green, im1_blue  = im1[:, :, 0], im1[:, :, 1], im1[:, :, 2]
+    im2_red, im2_green, im2_blue = im2[:, :, 0], im2[:, :, 1], im2[:, :, 2]
+    blend_im[:, :, 0], blend_im[:, :, 1], blend_im[:, :, 2] = \
+        pyramid_blending(im1_red, im2_red, mask, 10, 7, 5), \
+        pyramid_blending(im1_green, im2_green, mask, 10, 7, 5), \
+        pyramid_blending(im1_blue, im2_blue, mask, 10, 7, 5)
 
     plt.subplot(2, 2, 1)
     plt.imshow(im1)
@@ -196,39 +164,11 @@ def blending_example1():
     plt.subplot(2, 2, 4)
     plt.imshow(blend_im)
     plt.show()
-    return (im1, im2, mask, blend_im)
 
 
-def blending_example2():
-    im1 = read_image(relpath('externals/im11.jpg'), 2)
-    im2= read_image(relpath('externals/im22.jpg'), 2)
-    mask=read_image(relpath('externals/mask2.jpg'),1).astype(np.bool)
-    blend_im=np.zeros(im1.shape)
-    im1_R=im1[:,:,0]
-    im1_G=im1[:,:,1]
-    im1_B=im1[:,:,2]
-    im2_R = im2[:, :, 0]
-    im2_G = im2[:, :, 1]
-    im2_B = im2[:, :, 2]
-    blend_im[:,:,0],blend_im[:,:,1],blend_im[:,:,2]= pyramid_blending(im1_R,im2_R,mask,10,7,5),\
-                                                     pyramid_blending(im1_G,im2_G,mask,10,7,5),\
-                                                     pyramid_blending(im1_B,im2_B,mask,10,7,5)
-
-    plt.subplot(2, 2, 1)
-    plt.imshow(im1)
-    plt.subplot(2, 2, 2)
-    plt.imshow(im2)
-    plt.subplot(2, 2, 3)
-    plt.imshow(mask, cmap='gray')
-    plt.subplot(2, 2, 4)
-    plt.imshow(blend_im)
-    plt.show()
-    return (im1, im2, mask, blend_im)
-
-
-
-
-
-
-
-    
+if __name__ == "__main__":
+    example = int(input("Choose which example to run: 1 for Thor, 2 for Shark eating Tiger "))
+    if example == 1:
+        blending_example('externals/im1.jpg', 'externals/im2.jpg', 'externals/mask.jpg')
+    elif example == 2:
+        blending_example('externals/im11.jpg', 'externals/im22.jpg', 'externals/mask2.jpg')
